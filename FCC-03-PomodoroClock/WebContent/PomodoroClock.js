6b1f5cb2fc945122149c18899f8fa79adb6ee86a
DuaneGabel <duaneagabel@gmail.com>
2015-12-23 09:46:06 -0700

    further refinements to basic timer

--- FCC-03-PomodoroClock/WebContent/PomodoroClock.js
+++ FCC-03-PomodoroClock/WebContent/PomodoroClock.js
@@ -2,32 +2,73 @@ var sessionLength = 1;
 var breakLength = 5;
 var minutes = sessionLength - 1;
 var seconds = 60;
-//var interval = 	setInterval(startTimer(), 1000);
+//var interval = setInterval(displayTimer, 1000);
+var interval;
 
 function displayTimer() {
-	document.getElementById("timer").innerHTML = sessionLength + ":00";
+	document.getElementById("timer").innerHTML = checkTime(sessionLength) + ":00";
+	getBreak();
+	getSession();
 }
 
 function startTimer() {
-	minutes = checkTime(minutes);
-	seconds = checkTime(seconds);
-	setInterval(function() {
-		seconds--;
+	interval = setInterval(function() {
 
-		document.getElementById("timer").innerHTML = minutes + ":" + seconds;
-		
-		if (sec == 00) {
-			minutes--;
-			seconds = 60;
-	
-			if (minutes == 0) {
-				minutes = sessionLength;
+		if (seconds === 0 && minutes === 0) {
+			clearInterval(interval);
+		} else {
+			seconds--;
+
+			document.getElementById("timer").innerHTML = checkTime(minutes) + ":"
+					+ checkTime(seconds);
+
+			if (seconds === 0 && minutes > 0) {
+				minutes--;
+				seconds = 60;
 			}
 		}
 	}, 1000);
+	
+	displayTimer();
 }
 
+// minutes:seconds formatting function (00:00)
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+	if (i < 10) {
+		i = "0" + i
+	}
+	
+	return i;
 }
+
+function getBreak() {
+	document.getElementById("breakLength").innerHTML = breakLength;
+}
+
+function getSession() {
+	document.getElementById("sessionLength").innerHTML = sessionLength;
+}
+
+function decrement(i) {
+	if (i == "brkLn" && breakLength > 0) {
+		breakLength--;
+		getBreak();
+	} else if (i == "sessLn" && sessionLength > 0) {
+		sessionLength--;
+		getSession();
+	}
+	minutes = sessionLength - 1;
+	displayTimer();
+} 
+
+function increment(i) {
+	if (i == "brkLn") {
+		breakLength++;
+		getBreak();
+	} else if (i == "sessLn") {
+		sessionLength++;
+		getSession();
+	}
+	minutes = sessionLength - 1;
+	displayTimer();
+} 

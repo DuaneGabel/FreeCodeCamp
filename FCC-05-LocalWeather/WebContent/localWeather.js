1d9e62ef9e79b2b24e6fa3e3ca39445c053705f2
Duane Gabel <duaneagabel@gmail.com>
2017-09-07 22:39:04 +0000

    Resolved geolocation call to get lat/long, and added call to FCC weather API

--- FCC-05-LocalWeather/WebContent/localWeather.js
+++ FCC-05-LocalWeather/WebContent/localWeather.js
@@ -1,23 +1,34 @@
-var x = document.getElementById("demo");
-
-var latitude = "";
-var longitude = "";
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-
-    // latitude = position.coords.latitude;
-    // longitude = position.coords.longitude; 
-}
-
-// console.log("Latitude: " + latitude);
-// console.log("Longitude: " + longitude);
-
+angular.module('localWeatherApp', []).controller('localWeatherCtrl', function($scope, $http) {
+        
+    $scope.getWeather = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getPosition);
+        };
+        
+        function getPosition(position) {
+            $scope.lat = position.coords.latitude;
+            $scope.lon = position.coords.longitude;
+            
+            // console.log("lat: " + $scope.lat);
+            // console.log("lon: " + $scope.lon);
+            
+            var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + $scope.lat + "&lon=" + $scope.lon;
+            
+            $http.get(url).then(function(result) {
+                $scope.todaysDate = result.data.dt;
+                $scope.description = result.data.weather[0].description;
+                $scope.temp = result.data.main.temp;
+                $scope.humidity = result.data.main.humidity;
+                $scope.temp_min = result.data.main.temp_min;
+                $scope.temp_max = result.data.main.temp_max;
+                $scope.windSpeed = result.data.wind.speed + " mph";
+                // $scope.chanceOfRain = result.data.rain.3h;
+                $scope.clouds = result.data.clouds.all;
+                $scope.sunrise = result.data.sys.sunrise;
+                $scope.sunset = result.data.sys.sunset;
+            });
+        }
+        
+    };
+    
+});
